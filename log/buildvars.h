2560b0cf3801b0674d1879fbeef6d52993791936
Calvin Cheung <41496257+calvinc97@users.noreply.github.com>
2019-07-21 19:33:37 -0400

    update to latest firmware

--- log/buildvars.h
+++ log/buildvars.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013-2016 by Jacob Alexander
+/* Copyright (C) 2013-2017 by Jacob Alexander
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,25 +27,38 @@
 
 // ----- Defines -----
 
+// Helpers
+#define _STR_HELPER(x)
+#define _STR(x) _STR_HELPER(x)
+
 // You can change these to give your code its own name.
 #define STR_MANUFACTURER        L"Input Club"
-#define STR_PRODUCT             L"Keyboard - WhiteFox PartialMap pjrcUSB full"
-#define STR_SERIAL              L"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - mk20dx256vlh7"
+#define STR_PRODUCT             L"Keyboard - WhiteFox:truefox PixelMap USB"
+#define STR_SERIAL              L"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - mk20dx256"
+#define STR_CONFIG_NAME         L"xXXx"
 
 
 // Strings used in the CLI module
-#define CLI_Revision            "0a2b22d25f5bb4d515dd96faff48240dea45ccbc"
-#define CLI_Branch              "secure_disable"
+#define CLI_Revision            "f6d83f269246c937e2aede63d7f763f2c2b37483"
+#define CLI_RevisionNumber      853
+#define CLI_RevisionNumberStr   "853"
+#define CLI_Version             "0.4.9"
+#define CLI_VersionRevision     "2dd024f4a88ea3c53d1d3eac7e6dadcef3f04769"
+#define CLI_VersionRevNumber    723
+#define CLI_VersionRevNumberStr "723"
+#define CLI_Branch              "master"
 #define CLI_ModifiedStatus      "Clean"
 #define CLI_ModifiedFiles       "\r\n\t"
 #define CLI_RepoOrigin          "https://github.com/kiibohd/controller.git"
-#define CLI_CommitDate          "2017-10-04 09:38:29 -0700"
-#define CLI_CommitAuthor        "Jacob Alexander <haata@kiibohd.com>"
-#define CLI_Modules             "Scan(WhiteFox) Macro(PartialMap) Output(pjrcUSB) Debug(full)"
-#define CLI_BuildDate           "2019-07-21 19:32:44 +0000"
-#define CLI_BuildOS             "Linux-5.1.11-x86_64-linode127"
+#define CLI_CommitDate          "2018-04-12 14:49:12 -0700"
+#define CLI_CommitAuthor        "GitHub <noreply@github.com>"
+#define CLI_Modules             "Scan(WhiteFox:truefox) Macro(PixelMap) Output(USB) Debug(full)"
+#define CLI_BuildDate           "2019-07-21 23:22:23 +0000"
+#define CLI_BuildOS             "Ubuntu 14.04.5 LTS trusty"
+#define CLI_BuildCompiler       "/usr/bin/arm-none-eabi-gcc "
 #define CLI_Arch                "arm"
 #define CLI_Chip                "mk20dx256vlh7"
+#define CLI_ChipShort           "mk20dx256"
 #define CLI_CPU                 "cortex-m4"
 #define CLI_Device              "Keyboard"
 
@@ -56,5 +69,5 @@
 // match the INF file.
 #define VENDOR_ID               0x1C11
 #define PRODUCT_ID              0xB04D
-#define BCD_VERSION             514
+#define BCD_VERSION             853
 
